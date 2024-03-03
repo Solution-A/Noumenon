58548c47beaa8499b9dcdb70fd14e9694a98f959
Solution-A <danielcastrocruztf@gmail.com>
2024-03-03 16:35:04 +0000

    Add project files.

---
+++ Noumenon/Plugin.cs
@@ -0,0 +1,79 @@
+using Dalamud.Game.Command;
+using Dalamud.Interface.Windowing;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using Noumenon.Windows;
+using System.IO;
+
+namespace Noumenon
+{
+    public sealed class Plugin : IDalamudPlugin
+    {
+        public string Name => "Noumenon";
+        private const string CommandName = "/noumenon";
+
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private ICommandManager CommandManager { get; init; }
+        public Configuration Configuration { get; init; }
+        public WindowSystem WindowSystem = new("Noumenon");
+
+        private ConfigWindow ConfigWindow { get; init; }
+        private MainWindow MainWindow { get; init; }
+
+        public Plugin(
+            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+            [RequiredVersion("1.0")] ICommandManager commandManager)
+        {
+            this.PluginInterface = pluginInterface;
+            this.CommandManager = commandManager;
+
+            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Configuration.Initialize(this.PluginInterface);
+
+            // you might normally want to embed resources and load them from the manifest stream
+            //var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+            //var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
+
+            ConfigWindow = new ConfigWindow(this);
+            MainWindow = new MainWindow(this);
+            
+            WindowSystem.AddWindow(ConfigWindow);
+            WindowSystem.AddWindow(MainWindow);
+
+            this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "A useful message to display in /xlhelp"
+            });
+
+            this.PluginInterface.UiBuilder.Draw += DrawUI;
+            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+        }
+
+        public void Dispose()
+        {
+            this.WindowSystem.RemoveAllWindows();
+            
+            ConfigWindow.Dispose();
+            MainWindow.Dispose();
+            
+            this.CommandManager.RemoveHandler(CommandName);
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            MainWindow.IsOpen = true;
+        }
+
+        private void DrawUI()
+        {
+            this.WindowSystem.Draw();
+        }
+
+        public void DrawConfigUI()
+        {
+            ConfigWindow.IsOpen = true;
+        }
+    }
+}
---
+++ Noumenon/Windows/MainWindow.cs
@@ -0,0 +1,44 @@
+using Dalamud.Interface.Internal;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+using System;
+using System.Numerics;
+
+namespace Noumenon.Windows;
+
+public class MainWindow : Window
+{
+    private Plugin plugin;
+
+    public MainWindow(Plugin plugin) : base(
+        "Noumenon Design Manager", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    {
+        this.SizeConstraints = new WindowSizeConstraints
+        {
+            MinimumSize = new Vector2(375, 330),
+            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+        };
+
+        this.plugin = plugin;
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public override void Draw()
+    {
+        ImGui.Text($"The random config bool is {this.plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
+
+        if (ImGui.Button("Show Settings"))
+        {
+            this.plugin.DrawConfigUI();
+        }
+
+        ImGui.Spacing();
+
+        ImGui.Text("Have a goat:");
+        ImGui.Indent(55);
+        ImGui.Unindent(55);
+    }
+}
