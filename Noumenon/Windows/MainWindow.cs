aa0973792bb18d14e231ab2a9b03f744334a22fd
Solution-A <danielcastrocruztf@gmail.com>
2024-03-04 22:03:49 +0000

    Basic UI implementation. Provisional design list implemented.

--- Noumenon/Windows/MainWindow.cs
+++ Noumenon/Windows/MainWindow.cs
@@ -1,4 +1,5 @@
 using Dalamud.Interface.Internal;
+using Dalamud.Interface.Utility;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 using System;
@@ -15,7 +16,7 @@ public class MainWindow : Window
     {
         this.SizeConstraints = new WindowSizeConstraints
         {
-            MinimumSize = new Vector2(600, 600),
+            MinimumSize = new Vector2(600, 435),
             MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
         };
 
@@ -24,16 +25,31 @@ public class MainWindow : Window
 
     public override void Draw()
     {
-        ImGui.Text($"The random config bool is {this.plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
+        /*ImGui.Text($"The random config bool is {this.plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
 
         if (ImGui.Button("Show Settings"))
         {
             this.plugin.DrawConfigUI();
+        }*/
+        if (ImGui.BeginTable("designFrame", 2, ImGuiTableFlags.Borders | ImGuiTableFlags.Resizable))
+        {
+            ImGui.TableNextRow();
+            ImGui.TableSetColumnIndex(0);
+            ImGui.TableSetupColumn("designFrameColumn", ImGuiTableColumnFlags.None, 0f);
+            if (ImGui.BeginTable("designList", 1, ImGuiTableFlags.Borders | ImGuiTableFlags.ScrollY))
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    ImGui.TableNextRow();
+                    ImGui.TableSetColumnIndex(0);
+                    ImGui.Selectable("Design" + (i + 1));
+                }
+            }
+            ImGui.EndTable();
+            ImGui.TableSetColumnIndex(1);
+            ImGui.Text("DesignName");
         }
-        ImGui.Text("Hola");
-
-        ImGui.Indent(55);
-        ImGui.Unindent(55);
+        ImGui.EndTable();
     }
 
     public void Dispose(){ }
